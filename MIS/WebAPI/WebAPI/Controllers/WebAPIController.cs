9265a016b10d2a793560f2b783f04223421739a6
KINETICS\davidliu <davidliu@kinetics.com.tw>
2023-08-11 17:34:14 +0800

    0811

---
+++ MIS/WebAPI/WebAPI/Controllers/WebAPIController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebAPIController : ControllerBase
+    {
+        [HttpGet]
+        public string GetHello()
+        {
+            return "Hello The World";
+        }
+
+
+        [HttpGet("{id}")]
+        public string GetHelloWorld(int id)
+        {
+            //return "Hello The World....." + id.ToString();
+
+            return $"Hello The World...{id}";
+        }
+        [HttpGet("{a}/{b}")]
+        public string GetCompute(int a,int b)
+        {
+            int result = a+b;
+            return result.ToString();
+        }
+    }
+}
